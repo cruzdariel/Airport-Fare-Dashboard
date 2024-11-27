80fcf83bf632c2591336de2ea259d23758f7103a
Dariel Cruz Rodriguez <33551491+cruzdariel@users.noreply.github.com>
2024-11-26 19:15:34 -0500

    Initial commit

---
+++ airportfare.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+from duckduckgo_search import DDGS
+import requests
+
+data = pd.read_csv("/Users/dariel/Downloads/Airport Fare Dashboard/cleanairline.csv")
+
+def fetch_first_image(query):
+    with DDGS() as ddgs:
+        results = ddgs.images(query, max_results=1)
+        if results:
+            url = results[0]['image']
+            response = requests.head(url)
+            if response.status_code == 200:
+                return url
+            else:
+                return None
+        return None
+
+st.title("Average Airline Fare by Airport")
+
+airport = st.selectbox("Select an Airport:", data["Airport Name"])
+
+airport_data = data[data["Airport Name"] == airport]
+
+if not airport_data.empty:
+    fare = airport_data["Average Fare ($)"].values[0]
+    st.write(f"### Average Fare: ${fare:.2f}")
+    national_average = data["Average Fare ($)"].mean()
+    percent_difference = ((fare - national_average) / national_average) * 100
+    if percent_difference > 0:
+        st.write(f"#### That's {percent_difference:.2f}% <span style='color:red;'>higher</span> than the national average.", unsafe_allow_html=True)
+    else:
+        st.write(f"#### That's {np.abs(percent_difference):.2f}% <span style='color:green;'>lower</span>  than the national average.", unsafe_allow_html=True)
+
+else:
+    st.write("No data available for the selected airport.")
+
+image_url = fetch_first_image(airport)
+
+if image_url:
+    st.image(image_url, caption=f"Image of {airport}, sourced from {image_url}")
+else:
+    st.write("No image available for this airport.")
+
+st.write("*Data Source:* https://www.transtats.bts.gov/AverageFare/")
+#if st.checkbox("Show Full Data"):
+  #  st.dataframe(data)
