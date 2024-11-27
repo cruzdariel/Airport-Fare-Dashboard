9160bbf4cebc4a2f6408773b57fb7fa0ea10d310
Dariel Cruz Rodriguez <33551491+cruzdariel@users.noreply.github.com>
2024-11-27 02:27:04 -0500

    Update airportfare.py

--- airportfare.py
+++ airportfare.py
@@ -22,7 +22,7 @@ def fetch_first_image(query):
 st.title("Average Airline Fare by Airport")
 
 airport = st.selectbox("Select an Airport:", data["Airport Name"])
-year = st.selectbox("Select a year:", options=list(range(2014, 2025)))
+year = st.selectbox("Select a year:", options=list(range(2014, 2025)), index=10)
 
 airport_data = data[data["Airport Name"] == airport]
 
